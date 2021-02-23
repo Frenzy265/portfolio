6bde6d54de9ca196e440fcc2a4d6cca61a42d936
Frenzy265 <klaus_franziska@gmx.de>
2021-02-23 14:47:37 +0100

    💄 Create globalstyle file

---
+++ src/globalstyle.js
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from "styled-components/macro";
+
+const GlobalStyle = createGlobalStyle`
+
+*,
+*::after,
+*::before {
+    box-sizing: border-box;
+  }
+
+
+:root {
+--primary-color: #fff; // white
+--secondary-color: #000; // black
+--tertiary-color: #FEDE31; // yellow
+--default-box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.16)
+}
+
+#root {
+    height: 100%
+}
+
+html {
+    width: 100%;
+    height: 100%;
+}
+
+body {
+    margin: 0;
+    width: 100%;
+    background: var(--primary-color);
+}
+
+input, button, li {
+    font-size: 16px;
+    padding: 0px;
+}
+
+`;
+
+export default GlobalStyle;
