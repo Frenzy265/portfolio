1d740d3a22788d6c98477038003b00119c95fb97
Frenzy265 <klaus_franziska@gmx.de>
2021-03-04 18:13:23 +0100

    ✨ Add sections to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Header } from "./components/Header";
 import GlobalStyle from "./globalstyle";
+import About from "./sections/About";
+import Background from "./sections/Background";
+import Skills from "./sections/Skills";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>hello world</div>
+
+      <Header />
+      <About id="about" />
+      <Background id="background" />
+      <Skills id="skills" />
     </>
   );
 }
