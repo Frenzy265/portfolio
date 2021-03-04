441507b273ce157d90078aaa2b3a008e108530bb
Frenzy265 <klaus_franziska@gmx.de>
2021-03-04 16:44:20 +0100

    ⚡️ Add props to circle

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,6 +1,7 @@
 import styled from "styled-components/macro";
+import propTypes from "prop-types";
 
-const Circle = styled.div`
+const Container = styled.div`
   width: 500px;
   height: 500px;
   border-radius: 50%;
@@ -25,4 +26,29 @@ const Circle = styled.div`
   }
 `;
 
+const Circle = ({ title, imgSrc }) => {
+  return (
+    <Container>
+      {title ? (
+        <>
+          <h1>{title}</h1>
+          <svg viewBox="0 0 100 100">
+            <path
+              d="M3.375,53.375a50,50,0,1,0,50-50A49.992,49.992,0,0,0,3.375,53.375Zm58.774,0L42.462,33.88a4.64,4.64,0,1,1,6.563-6.562l22.933,23a4.634,4.634,0,0,1,.144,6.394L49.5,79.385a4.632,4.632,0,1,1-6.563-6.538Z"
+              transform="translate(-3.375 -3.375)"
+            />
+          </svg>
+        </>
+      ) : (
+        <img src={imgSrc} alt="Emoji of me" />
+      )}
+    </Container>
+  );
+};
+
 export default Circle;
+
+Circle.propTypes = {
+  title: propTypes.string,
+  imgSrc: propTypes.any,
+};
--- src/components/Circle.stories.js
+++ src/components/Circle.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Circle from "./Circle";
+import Emoji from "../assets/emoji.png";
 
 export default {
   title: "Example/Circle",
@@ -7,14 +8,5 @@ export default {
   parameters: { layout: "centered" },
 };
 
-export const circle = () => (
-  <Circle>
-    <h1>Skills</h1>
-    <svg viewBox="0 0 100 100">
-      <path
-        d="M3.375,53.375a50,50,0,1,0,50-50A49.992,49.992,0,0,0,3.375,53.375Zm58.774,0L42.462,33.88a4.64,4.64,0,1,1,6.563-6.562l22.933,23a4.634,4.634,0,0,1,.144,6.394L49.5,79.385a4.632,4.632,0,1,1-6.563-6.538Z"
-        transform="translate(-3.375 -3.375)"
-      />
-    </svg>
-  </Circle>
-);
+export const titleCircle = () => <Circle title="skills" />;
+export const imageCircle = () => <Circle imgSrc={Emoji} />;
