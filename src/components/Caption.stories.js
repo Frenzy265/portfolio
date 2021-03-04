3e9a04dc7dcb0dcbbd9bf9572395b69c3d628487
Frenzy265 <klaus_franziska@gmx.de>
2021-03-04 15:36:58 +0100

    💄 Add caption and stories

---
+++ src/components/Caption.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Caption } from "./Caption";
+
+export default {
+  title: "Example/Caption",
+  component: Caption,
+  parameters: { layout: "centered" },
+};
+
+export const caption = () => <Caption />;
