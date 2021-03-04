70afa26b1a6f36761656c69fb66f49544ce50ca7
Frenzy265 <klaus_franziska@gmx.de>
2021-03-04 18:13:10 +0100

    ✨ Create section skills

---
+++ src/sections/Skills.js
@@ -0,0 +1,14 @@
+import Section from "../components/Section";
+import { SkillsList } from "../components/SkillsList";
+import Circle from "../components/Circle";
+
+const Skills = () => {
+  return (
+    <Section>
+      <Circle title="skills" />
+      <SkillsList />
+    </Section>
+  );
+};
+
+export default Skills;
