01f443a1361f3f77747af4965c9fd26a4061695c
Frenzy265 <klaus_franziska@gmx.de>
2021-03-09 15:55:37 +0100

    💄 Increase contact card

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,17 +1,18 @@
 import styled from "styled-components/macro";
 
 const ContactCard = styled.div`
-  height: 150px;
-  width: 150px;
+  height: 200px;
+  width: 200px;
   border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   background: var(--secondary-color);
   color: var(--primary-color);
-  font-size: 3rem;
+  font-size: 4rem;
   box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.16);
   cursor: pointer;
+  grid-auto-flow: column;
 
   :hover,
   :active {
