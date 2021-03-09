e46d0f288e9eb4be4a302014d839544a22016eaa
Frenzy265 <klaus_franziska@gmx.de>
2021-03-09 15:18:42 +0100

    💄 Change size and add active prop

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,19 +1,20 @@
 import styled from "styled-components/macro";
 
 const ContactCard = styled.div`
-  height: 100px;
-  width: 100px;
+  height: 150px;
+  width: 150px;
   border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   background: var(--secondary-color);
   color: var(--primary-color);
-  font-size: 2rem;
+  font-size: 3rem;
   box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.16);
   cursor: pointer;
 
-  :hover {
+  :hover,
+  :active {
     background: ${(props) => props.hoverColor};
   }
 `;
