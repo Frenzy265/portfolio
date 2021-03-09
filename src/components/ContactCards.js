4b08c36a40c52be90653502ed82e913def5ea90c
Frenzy265 <klaus_franziska@gmx.de>
2021-03-09 14:44:21 +0100

    💄 Create contact cards with content and stories

---
+++ src/components/ContactCards.js
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components/macro";
+import ContactCard from "./ContactCard";
+import * as FaIcon from "react-icons/fa";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3fr);
+  grid-column-gap: 10px;
+  grid-auto-flow: column;
+`;
+
+const ContactDetails = [
+  {
+    title: "Xing",
+    icon: <FaIcon.FaXing />,
+    color: "#007877",
+    link: "https://www.xing.com/profile/Franziska_Klaus2/cv",
+  },
+  {
+    title: "Github",
+    icon: <FaIcon.FaGithub />,
+    color:
+      "radial-gradient(circle, var(--secondary-color) 18%, var(--primary-color) 18%)",
+    link: "https://github.com/Frenzy265",
+  },
+
+  {
+    title: "LinkedIn",
+    icon: <FaIcon.FaLinkedinIn />,
+    color: "#005DC9",
+    link: "https://linkedin.com/in/frenzy265",
+  },
+];
+
+const ContactCards = () => {
+  return (
+    <Container>
+      {ContactDetails.map((detail, index) => (
+        <a key={index} href={detail.link}>
+          <ContactCard hoverColor={detail.color}>{detail.icon}</ContactCard>
+        </a>
+      ))}
+    </Container>
+  );
+};
+
+export default ContactCards;
---
+++ src/components/ContactCards.stories.js
@@ -0,0 +1,9 @@
+import ContactCards from "./ContactCards";
+
+export default {
+  title: "Example/ContactCards",
+  component: ContactCards,
+  parameters: { layout: "centered" },
+};
+
+export const contactCards = () => <ContactCards />;
