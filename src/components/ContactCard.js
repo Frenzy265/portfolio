79747a04b349401fef22c8d4bd65839eb603248d
Frenzy265 <klaus_franziska@gmx.de>
2021-03-09 16:49:27 +0100

    💄 Downsize contact card

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,15 +1,15 @@
 import styled from "styled-components/macro";
 
 const ContactCard = styled.div`
-  height: 200px;
-  width: 200px;
+  height: 100px;
+  width: 100px;
   border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   background: var(--secondary-color);
   color: var(--primary-color);
-  font-size: 4rem;
+  font-size: 2rem;
   box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.16);
   cursor: pointer;
   grid-auto-flow: column;
