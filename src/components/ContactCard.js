c6edb3317630cf99a130b9bcb7dc13f32dd9ee6b
Frenzy265 <klaus_franziska@gmx.de>
2021-03-09 14:43:55 +0100

    💄 Create contact card

---
+++ src/components/ContactCard.js
@@ -0,0 +1,21 @@
+import styled from "styled-components/macro";
+
+const ContactCard = styled.div`
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: var(--secondary-color);
+  color: var(--primary-color);
+  font-size: 2rem;
+  box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.16);
+  cursor: pointer;
+
+  :hover {
+    background: ${(props) => props.hoverColor};
+  }
+`;
+
+export default ContactCard;
