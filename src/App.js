3f97e9f0c8fbbc19a465a2e9b619220737b0f48e
Frenzy265 <klaus_franziska@gmx.de>
2021-03-11 14:27:32 +0100

    ⚡️ Add wrapper

--- src/App.js
+++ src/App.js
@@ -6,17 +6,22 @@ import About from "./sections/About";
 import Background from "./sections/Background";
 import Projects from "./sections/Projects";
 import Skills from "./sections/Skills";
+import { Wrapper } from "./components/Wrapper";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <About />
-      <Background />
-      <Skills />
-      <Projects />
-      <Footer />
+      <Wrapper>
+        <Header />
+        <main>
+          <About />
+          <Background />
+          <Skills />
+          <Projects />
+        </main>
+        <Footer />
+      </Wrapper>
     </>
   );
 }
---
+++ src/components/Wrapper.js
@@ -0,0 +1,25 @@
+import styled from "styled-components/macro";
+
+export const Wrapper = styled.div`
+  height: 100vh;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas:
+    "header"
+    "main"
+    "footer";
+
+  header {
+    grid-area: header;
+  }
+
+  main {
+    grid-area: main;
+  }
+
+  footer {
+    grid-area: footer;
+  }
+`;
