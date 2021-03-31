4ed3ab5626762863d53cd8d6e9e3cd0ff6c2d410
Frenzy265 <klaus_franziska@gmx.de>
2021-03-31 12:26:55 +0200

    💄 Change image

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,12 +1,12 @@
 import Section from "../components/Section";
 import Circle from "../components/Circle";
 import { TextBox } from "../components/Textbox";
-import Emoji from "../assets/emoji.png";
+import ProfilePicture from "../assets/profil.jpeg";
 
 const About = () => {
   return (
     <Section id="about">
-      <Circle imgSrc={Emoji} />
+      <Circle imgSrc={ProfilePicture} />
       <TextBox />
     </Section>
   );
